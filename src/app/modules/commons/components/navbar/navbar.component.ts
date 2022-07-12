4deb79df85aa719ee05a72d1d35172762d917167
Paweł Białobrodzki <91831767+RarelyUseful@users.noreply.github.com>
2022-07-12 17:39:33 +0200

    Container (#4)
    * folders structure added, routing added
    
    * added global styling, added navbar, changes in homepage component

--- src/app/modules/commons/components/navbar/navbar.component.ts
+++ src/app/modules/commons/components/navbar/navbar.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  public title: string = 'SoftwareHut Hiring';
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public bellButton(): void {
+    console.log('bell button pressed');
   }
-
+  public userButton(): void {
+    alert('user button pressed');
+  }
 }
