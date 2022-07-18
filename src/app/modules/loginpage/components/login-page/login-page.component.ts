ffd48bd24244f26fe2b370db8155e72beca7c205
olgaGuberska <92678951+olgaGuberska@users.noreply.github.com>
2022-07-18 18:31:20 +0200

    login works/forgot password (#34)

--- src/app/modules/loginpage/components/login-page/login-page.component.ts
+++ src/app/modules/loginpage/components/login-page/login-page.component.ts
@@ -1,10 +1,7 @@
-
 import { Component, OnChanges, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/modules/commons/services/auth/auth.service';
 import { AxiosService } from '../../../commons/services/axios/axios.service';
-
-
 
 @Component({
   selector: 'app-login-page',
@@ -15,19 +12,22 @@ export class LoginPageComponent implements OnChanges, OnInit {
   public password: string = 'password';
   public email: string = '0';
 
-
   constructor(private _fb: FormBuilder, private _auth: AuthService) {}
   public loginForm = this._fb.group({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [
       Validators.required,
-      Validators.minLength(8),
+      Validators.minLength(5),
     ]),
   });
 
-  ngOnChanges(): void { }
+  ngOnChanges(): void {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.email);
+    this.email = this.loginForm.value.email;
+    console.log(this.email);
+  }
 
   async signIn() {
     this.email = this.loginForm.value.email;
@@ -37,4 +37,8 @@ export class LoginPageComponent implements OnChanges, OnInit {
 
   public onSubmit(form: any) {}
 
+  public getNewPassword() {
+    this.email = this.loginForm.value.email;
+    this._auth.isUserExist(this.email);
+  }
 }
