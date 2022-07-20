a76af83673f5de5ba0b88219a3601528f0a6c2db
olgaGuberska <olga.priv@gmail.com>
2022-07-20 13:45:20 +0200

    rxjs autocomplete

--- src/app/modules/projects/components/projects/projects.component.ts
+++ src/app/modules/projects/components/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
+import { ProjectsService } from 'src/app/modules/commons/services/projects/projects.service';
 // import { }
 
 
@@ -13,13 +14,14 @@ export class ProjectsComponent implements OnInit {
   public componentName: string = "projects";
   public isEditCreateProject: boolean = false;
 
-  constructor(private _router: Router) {
+  constructor(private _router: Router, private _projectService: ProjectsService) {
 
   }
 
   ngOnInit(): void {}
   testFunc() {
     alert('button pressed');
+    
   }
 
   moveToCreateProject() {
