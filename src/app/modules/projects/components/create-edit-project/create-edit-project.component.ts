f9a38002c27b08c8bed10983a19a54f0e5836685
olgaGuberska <olga.priv@gmail.com>
2022-07-20 16:09:01 +0200

    adding deleteng skills/form controls for inputs

--- src/app/modules/projects/components/create-edit-project/create-edit-project.component.ts
+++ src/app/modules/projects/components/create-edit-project/create-edit-project.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { FormControl } from '@angular/forms';
 import { Observable, startWith } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Skill } from 'src/app/modules/commons/interfaces/Skill';
 import { ProjectsService } from 'src/app/modules/commons/services/projects/projects.service';
-
 
 export interface User {
   name: string;
@@ -16,62 +15,52 @@ export interface User {
   templateUrl: './create-edit-project.component.html',
   styleUrls: ['./create-edit-project.component.scss'],
 })
-
 export class CreateEditProjectComponent implements OnChanges, OnInit {
+  
   public myControl = new FormControl('');
-
   public textHeader: string = 'Create/Edit project';
   public textBody: string = 'Skills';
   public ratingArray: Array<number> = [];
   public totalStar: number = 5;
   public rating: number = 2;
-
   public filteredOptions!: Observable<Array<Skill>>;
-  public listOfSkills: Array<Skill> = [
-    { id: 1, name: 'string1' },
-    { id: 2, name: 'string2' },
-  ];
   public listOfSkillsForProject: Array<Skill> = [];
+  public textareaValue: string = '';
+  public listOfSkills: Array<Skill> = [];
+  //   { id: 1, name: 'string1' },
+  //   { id: 2, name: 'string2' },
+  // ];
 
-  ngOnChanges(changes: SimpleChanges): void {}
-  // constructor(private fb: FormBuilder, private _projectService: ProjectsService) {
-  //   this._projectService.projectSkills$.subscribe({
-  //     next: (data) => {
-  //       console.log(data);
-  //       this.obsSkillsList = data;
-  //     },
-  //   });
+  ngOnChanges(): void {}
 
-  //   this.filteredOptions = this.myControl.valueChanges.pipe(
-  //     startWith(''),
-  //     map(el => this._filter(el)),
-  //   );
-  // }
-
-  constructor(
-    private fb: FormBuilder,
-    private _projectService: ProjectsService
+  constructor(private fb: FormBuilder, private _projectService: ProjectsService
   ) {
     this._projectService.projectSkills$.subscribe({
       next: (data) => {
         this.listOfSkills = data;
-        console.log(this.listOfSkills);
       },
     });
   }
 
-
   public projectForm = this.fb.group({
-    login: [''],
-    password: [''],
+    projectName: [''],
+    seniority: [''],
+    from: [''],
+    to: [''],
+    location: [''],
+    // login: [''],
+    // password: [''],
   });
 
+  public textareaForm = this.fb.group({
+    textarea: new FormControl(''),
+  });
   
+
   ngOnInit() {
     for (let index = 0; index < this.totalStar; index++) {
       this.ratingArray.push(index);
     }
-
 
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
@@ -81,12 +70,10 @@ export class CreateEditProjectComponent implements OnChanges, OnInit {
 
   private _filter(value: string): Array<Skill> {
     const filterValue = value.toLowerCase();
-    console.log(value);
 
     return this.listOfSkills.filter((option) =>
       option.name.toLowerCase().includes(filterValue)
     );
-
   }
 
   calculateRating(newRating: number) {
@@ -101,36 +88,32 @@ export class CreateEditProjectComponent implements OnChanges, OnInit {
     }
   }
 
-
   onSelection(choosenSkill: Skill) {
-    console.log(choosenSkill);
-
-    console.log(this.listOfSkillsForProject);
-    if (this.listOfSkillsForProject.length !== 0) {
-console.log(this.listOfSkillsForProject)
-      // for (let i = 0; i < this.listOfSkillsForProject.length; i++) {
-      for (const skill of this.listOfSkillsForProject) {
-        if (skill.name === choosenSkill.name) {
-          alert('Already on the list');
-        } else {
-          console.log('from else ');
-          this.listOfSkillsForProject.push(choosenSkill);
-        }
-      }
    
-       
-      // }
-
+    if (this.listOfSkillsForProject.length !== 0) {
+      let nameListOfSkillProject = this.listOfSkillsForProject.map((el) => {
+        return el.name;
+      });
+     
+      if (!nameListOfSkillProject.includes(choosenSkill.name)) {
+        this.listOfSkillsForProject.push(choosenSkill);
+      } else {
+        alert('Skill already choosen');
+      }
     } else {
       this.listOfSkillsForProject.push(choosenSkill);
     }
   }
 
   removeSkillFromProject(skillToRemove: Skill) {
-
     const updatedProjectSkils = this.listOfSkillsForProject.filter((skill) => {
       return skill !== skillToRemove;
     });
     this.listOfSkillsForProject = updatedProjectSkils;
+  }
+
+  saveProjectChanges() {
+
+console.log(this.textareaForm.value.textarea)
   }
 }
