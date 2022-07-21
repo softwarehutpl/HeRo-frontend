92a332ca756b8064c7e5540cc4f5e314bfc91cff
olgaGuberska <olga.priv@gmail.com>
2022-07-21 10:14:25 +0200

    removing console.logs

--- src/app/modules/commons/services/auth/auth.service.ts
+++ src/app/modules/commons/services/auth/auth.service.ts
@@ -1,57 +1,40 @@
-
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import axios from 'axios';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  public urlAuth: string = 'https://swh-t-praktyki2022-app.azurewebsites.net/Auth/SignIn';
+  public urlRecoveryPassword: string = 'https://swh-t-praktyki2022-app.azurewebsites.net/Auth/PasswordRecoveryMail';
 
-  public urlAuth: string = "https://swh-t-praktyki2022-app.azurewebsites.net/Auth/SignIn";
-  public urlRecoveryPassword: string = "https://swh-t-praktyki2022-app.azurewebsites.net/Auth/PasswordRecoveryMail";
- 
-  
-
-  constructor(private _router: Router) {} 
-
+  constructor(private _router: Router) {}
 
   public async isAuth(password: string, email: string) {
-    
-      await axios.post(this.urlAuth, {password: password, email: email}, {
-        withCredentials: true
-      })
-      .then( res => {
-        if (res.statusText === 'OK') {
-          console.log(res)
-          this.getCandidates() 
-          return this._router.navigate(['/home'])
-        } 
-        this.getCandidates() 
-        console.log(res)
-        return 
-      })
-  }
-
-  getCandidates() {
-
-
-    axios.get('https://swh-t-praktyki2022-app.azurewebsites.net/Skill/GetList', {withCredentials: true})
-    .then(res => console.log(res))
-
-  }
-
-  isUserExist(userEmail: string) {
-    axios.post(this.urlRecoveryPassword, null, {params: {email: userEmail}})
-      .then(res => { 
-        if(res.status === 200) {
-
+    await axios
+      .post(
+        this.urlAuth,
+        { password: password, email: email },
+        {
+          withCredentials: true,
         }
-      console.log(res.status)})
+      )
+      .then((res) => {
+        if (res.statusText === 'OK') {
+          return this._router.navigate(['/home']);
+        }
+        return;
+      });
   }
 
-
-
+  public isUserExist(userEmail: string) {
+    axios
+      .post(this.urlRecoveryPassword, null, { params: { email: userEmail } })
+      .then((res) => {
+        if (res.status === 200) {
+        }
+        console.log(res.status);
+      });
+  }
 }
-
