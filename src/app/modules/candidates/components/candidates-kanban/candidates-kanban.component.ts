1a28e15f823486d01e3c4114861344b349ea7f26
Paweł Białobrodzki <bialobrodzki@gmail.com>
2022-07-26 12:18:33 +0200

    kanban now downloads one larger file and sorts it locally

--- src/app/modules/candidates/components/candidates-kanban/candidates-kanban.component.ts
+++ src/app/modules/candidates/components/candidates-kanban/candidates-kanban.component.ts
@@ -13,30 +13,56 @@ import { Candidate } from '../../CandidatesInterface';
   styleUrls: ['./candidates-kanban.component.scss'],
 })
 export class CandidatesKanbanComponent implements OnInit {
-  newCand!: Candidate[];
-  evaluation!: Candidate[];
-  interview!: Candidate[];
-  phoneInterview!: Candidate[];
-  techInterview!: Candidate[];
-  offer!: Candidate[];
-  hired!: Candidate[];
-  dropped!: Candidate[];
+  newCand: Candidate[] = [];
+  evaluation: Candidate[] = [];
+  interview: Candidate[] = [];
+  phoneInterview: Candidate[] = [];
+  techInterview: Candidate[] = [];
+  offer: Candidate[] = [];
+  hired: Candidate[] = [];
+  dropped: Candidate[] = [];
 
   constructor(private service: CandidatesDataService) {}
 
   async ngOnInit() {
-    this.service.newCandidates.subscribe((result) => (this.newCand = result));
-    this.service.evaluation.subscribe((result) => (this.evaluation = result));
-    this.service.interview.subscribe((result) => (this.interview = result));
-    this.service.phoneInterview.subscribe(
-      (result) => (this.phoneInterview = result)
-    );
-    this.service.techInterview.subscribe(
-      (result) => (this.techInterview = result)
-    );
-    this.service.offer.subscribe((result) => (this.offer = result));
-    this.service.hired.subscribe((result) => (this.hired = result));
-    this.service.dropped.subscribe((result) => (this.dropped = result));
+    this.service.allCandidates.subscribe((result) => {
+      console.log('Reading result:');
+      console.log(result[0]);
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].status == 'NEW') {
+          this.newCand.push(result[i]);
+        } else if (result[i].status == 'HIRED') {
+          this.hired.push(result[i]);
+        } else if (result[i].status == 'DROPPED_OUT') {
+          this.dropped.push(result[i]);
+        } else if (result[i].status == 'IN_PROCESSING') {
+          /* later put inner loop of stages below */
+        }
+        if (result[i].stage == 'EVALUATION') {
+          this.evaluation.push(result[i]);
+        } else if (result[i].stage == 'INTERVIEW') {
+          this.interview.push(result[i]);
+        } else if (result[i].stage == 'PHONE_INTERVIEW') {
+          this.phoneInterview.push(result[i]);
+        } else if (result[i].stage == 'TECH_INTERVIEW') {
+          this.techInterview.push(result[i]);
+        } else if (result[i].stage == 'OFFER') {
+          this.offer.push(result[i]);
+        }
+      }
+    });
+    // this.service.newCandidates.subscribe((result) => (this.newCand = result));
+    // this.service.evaluation.subscribe((result) => (this.evaluation = result));
+    // this.service.interview.subscribe((result) => (this.interview = result));
+    // this.service.phoneInterview.subscribe(
+    //   (result) => (this.phoneInterview = result)
+    // );
+    // this.service.techInterview.subscribe(
+    //   (result) => (this.techInterview = result)
+    // );
+    // this.service.offer.subscribe((result) => (this.offer = result));
+    // this.service.hired.subscribe((result) => (this.hired = result));
+    // this.service.dropped.subscribe((result) => (this.dropped = result));
   }
 
   drop(event: CdkDragDrop<Candidate[]>) {
--- src/app/modules/candidates/services/candidates-data.service.ts
+++ src/app/modules/candidates/services/candidates-data.service.ts
@@ -12,66 +12,71 @@ export class CandidatesDataService {
   private _candidates: BehaviorSubject<Candidate[]> = new BehaviorSubject(
     [] as Candidate[]
   );
+  private _candidatesAll: Promise<Candidate[]> = this.getAllCandidates();
 
-  private _newCandidates: Promise<Candidate[]> = this.getCandidatesByStatus([
-    'NEW',
-  ]);
-  private _hired: Promise<Candidate[]> = this.getCandidatesByStatus(['HIRED']);
-  private _dropped: Promise<Candidate[]> = this.getCandidatesByStatus([
-    'DROPPED_OUT',
-  ]);
-  private _evaluation: Promise<Candidate[]> = this.getCandidatesByStatus(
-    [],
-    ['EVALUATION']
-  );
-  private _interview: Promise<Candidate[]> = this.getCandidatesByStatus(
-    [],
-    ['INTERVIEW']
-  );
-  private _phoneInterview: Promise<Candidate[]> = this.getCandidatesByStatus(
-    [],
-    ['PHONE_INTERVIEW']
-  );
-  private _techInterview: Promise<Candidate[]> = this.getCandidatesByStatus(
-    [],
-    ['TECH_INTERVIEW']
-  );
-  private _offer: Promise<Candidate[]> = this.getCandidatesByStatus(
-    [],
-    ['OFFER']
-  );
+  // private _newCandidates: Promise<Candidate[]> = this.getCandidatesByStatus([
+  //   'NEW',
+  // ]);
+  // private _hired: Promise<Candidate[]> = this.getCandidatesByStatus(['HIRED']);
+  // private _dropped: Promise<Candidate[]> = this.getCandidatesByStatus([
+  //   'DROPPED_OUT',
+  // ]);
+  // private _evaluation: Promise<Candidate[]> = this.getCandidatesByStatus(
+  //   [],
+  //   ['EVALUATION']
+  // );
+  // private _interview: Promise<Candidate[]> = this.getCandidatesByStatus(
+  //   [],
+  //   ['INTERVIEW']
+  // );
+  // private _phoneInterview: Promise<Candidate[]> = this.getCandidatesByStatus(
+  //   [],
+  //   ['PHONE_INTERVIEW']
+  // );
+  // private _techInterview: Promise<Candidate[]> = this.getCandidatesByStatus(
+  //   [],
+  //   ['TECH_INTERVIEW']
+  // );
+  // private _offer: Promise<Candidate[]> = this.getCandidatesByStatus(
+  //   [],
+  //   ['OFFER']
+  // );
 
   constructor() {
     this.getCandidatesForList();
+    // this.getAllCandidates();
   }
 
   get candidates() {
     return this._candidates.asObservable();
   }
-  get newCandidates() {
-    return from(this._newCandidates);
+  get allCandidates() {
+    return from(this._candidatesAll);
   }
-  get hired() {
-    return from(this._hired);
-  }
-  get dropped() {
-    return from(this._dropped);
-  }
-  get evaluation() {
-    return from(this._evaluation);
-  }
-  get interview() {
-    return from(this._interview);
-  }
-  get phoneInterview() {
-    return from(this._phoneInterview);
-  }
-  get techInterview() {
-    return from(this._techInterview);
-  }
-  get offer() {
-    return from(this._offer);
-  }
+  // get newCandidates() {
+  //   return from(this._newCandidates);
+  // }
+  // get hired() {
+  //   return from(this._hired);
+  // }
+  // get dropped() {
+  //   return from(this._dropped);
+  // }
+  // get evaluation() {
+  //   return from(this._evaluation);
+  // }
+  // get interview() {
+  //   return from(this._interview);
+  // }
+  // get phoneInterview() {
+  //   return from(this._phoneInterview);
+  // }
+  // get techInterview() {
+  //   return from(this._techInterview);
+  // }
+  // get offer() {
+  //   return from(this._offer);
+  // }
 
   //paginator settings:
   public pageIndex = 0;
@@ -119,7 +124,7 @@ export class CandidatesDataService {
     stage?: string[]
     //sort?: string
   ): Promise<Candidate[]> {
-    console.log('Fetching Candidates by status');
+    // console.log('Fetching Candidates by status');
     const URL =
       'https://swh-t-praktyki2022-app.azurewebsites.net/Candidate/GetList';
     const headers = new HttpHeaders({ accept: 'application/json' });
@@ -149,6 +154,36 @@ export class CandidatesDataService {
       .then((res) => {
         if (res.statusText === 'OK') {
           console.log(res);
+          return res.data.candidateInfoForListDTOs;
+        } else {
+          console.log('Error, status not OK');
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  @useMocks(false, import(`@mocks/candidates.json`))
+  public async getAllCandidates(): Promise<Candidate[]> {
+    // console.log('Fetching All Candidates');
+    const URL =
+      'https://swh-t-praktyki2022-app.azurewebsites.net/Candidate/GetList';
+    const headers = new HttpHeaders({ accept: 'application/json' });
+    const body = {
+      paging: {
+        pageSize: 20, //max 20 elements to reduce clutter
+        pageNumber: 1, //API requirement
+      },
+    };
+    const Options = {
+      header: headers,
+      withCredentials: true,
+    };
+
+    return await axios
+      .post(URL, body, Options)
+      .then((res) => {
+        if (res.statusText === 'OK') {
+          // console.log(res);
           return res.data.candidateInfoForListDTOs;
         } else {
           console.log('Error, status not OK');
