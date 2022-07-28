f77667f9ea53186780c2fd4e918cad0eb8c8854a
olgaGuberska <92678951+olgaGuberska@users.noreply.github.com>
2022-07-28 12:47:08 +0200

    Create new project (#77)
    * create new/edit works - lack of router
    
    * cleaning
    
    * working on checkbox component
    
    * starus/stages from requests

--- src/app/modules/candidates/components/candidates/candidates.component.ts
+++ src/app/modules/candidates/components/candidates/candidates.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class CandidatesComponent {
   public componentName = 'candidates';
   public isAutocomplete = false;
+
+  public isStage = true;
+  constructor() {}
+
+  ngOnInit(): void {}
+
 }
--- src/app/modules/commons/components/filter/filter.component.ts
+++ src/app/modules/commons/components/filter/filter.component.ts
@@ -1,32 +1,75 @@
-import { Component, Input, OnChanges, } from '@angular/core';
-import { Filter, Data } from '../definition';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Subfilter, Data } from '../definition';
 import { FiltersService } from '../../services/filters/filters.service';
 import { FormBuilder } from '@angular/forms';
+import { StageStatusData } from '../../interfaces/filters';
+
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent implements OnChanges {
+export class FilterComponent implements OnChanges, OnInit {
   @Input() public whichComponentRender = '';
   @Input() public isAutocomplete?: boolean;
+  @Input() public isStage?: boolean;
 
   public selected = 'all';
-  public filters?: Array<Filter>;
+  public filters?: Subfilter[];
   public sidebarButton1 = Data.sidebarButton1;
   public sidebarButton2 = Data.sidebarButton2;
   public sidebarButton3 = Data.sidebarButton3;
+  public checkboxLabels = { status: 'Status', stage: 'Stage' };
+  public stageToSubfilter!: Subfilter[];
+  public statusToSubfilter!: Subfilter[];
+  public statusColor = 'STATUS';
+
   // public checkboxForm =  this._fb.group({
 
   //     })
-  constructor(private _filterService: FiltersService, private _fb: FormBuilder) {
-  
-  }
+  constructor(
+    private _filterService: FiltersService,
+    private _fb: FormBuilder
+  ) {}
+
 
  ngOnChanges(): void {
     this.filters = this._filterService.filtersForComponent(
       this.whichComponentRender
     );
+  }
+
+  async ngOnInit() {
+    const dataForCheckboxCandidates =
+      await this._filterService.getStageAndStatusList();
+    console.log(dataForCheckboxCandidates);
+    this.convertCandidatesCacboxDTOToSubfilter(dataForCheckboxCandidates);
+    if (this.whichComponentRender === 'projects') {
+this.statusToSubfilter = [{name: "Open", checked: true, color: "status"}, {name: "Closed", checked: true, color: "status"}];
+    }
+  }
+
+  public convertCandidatesCacboxDTOToSubfilter(
+    dataDTOCheckbox: StageStatusData
+  ) {
+    this.stageToSubfilter = [];
+    this.statusToSubfilter = [];
+    const stageSubfilter = dataDTOCheckbox.stage.map((el) => {
+      const checkboxObject = {
+        name: el,
+        checked: true,
+        color: el,
+      };
+      this.stageToSubfilter.push(checkboxObject);
+    });
+    const statusSubfilter = dataDTOCheckbox.status.map((el) => {
+      const checkboxObject = {
+        name: el,
+        checked: true,
+        color: 'status',
+      };
+      this.statusToSubfilter.push(checkboxObject);
+    });
   }
 }
