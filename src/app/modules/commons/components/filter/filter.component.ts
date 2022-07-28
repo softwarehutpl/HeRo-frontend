55ecb9e197fa13f37475f1cccc6da17e976bc907
olgaGuberska <olga.priv@gmail.com>
2022-07-28 09:47:33 +0200

    autoccomplete project works

--- src/app/modules/commons/components/filter/filter.component.ts
+++ src/app/modules/commons/components/filter/filter.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnChanges, } from '@angular/core';
+import { Component, Input, OnChanges,  } from '@angular/core';
 import { Filter, Data } from '../definition';
 import { FiltersService } from '../../services/filters/filters.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
 import { ProjectListoToAutocomplete } from '../../mockups/mock-projects';
 import { ProjectsService } from '../../services/projects/projects.service';
+import { Observable, startWith, map } from 'rxjs';
+
 
 
 @Component({
@@ -21,22 +23,62 @@ export class FilterComponent implements OnChanges {
   public sidebarButton2 = Data.sidebarButton2;
   public sidebarButton3 = Data.sidebarButton3;
   public listOfProjects: ProjectListoToAutocomplete[] = [];
+  // public listOfProjects
+  public autocompleteForm = new FormControl('');
+  public projectAutocompleteOptions!: Observable<ProjectListoToAutocomplete[]>
   // public checkboxForm =  this._fb.group({
 
   //     })
-  constructor(private _filterService: FiltersService, private _fb: FormBuilder, private _projetService: ProjectsService) {
-    this._filterService.projectList$.subscribe({
+  constructor(public filterService: FiltersService, private _fb: FormBuilder, private _projetService: ProjectsService) {
+    this.filterService.projectList$.subscribe({
       next: (data) => {
         console.log(data)
         this.listOfProjects = data;
+        console.log(this.listOfProjects)
       }
     })
   }
 
  ngOnChanges(): void {
-    this.filters = this._filterService.filtersForComponent(
+    this.filters = this.filterService.filtersForComponent(
       this.whichComponentRender
     );
   }
+  
+  ngOnInit() {
+    // this.listOfProjects.asObservable()
+    this.projectAutocompleteOptions= this.autocompleteForm.valueChanges.pipe(
+      startWith(''),
+      map((el) => this._filter(el))
+    );
+  }
+
+  private _filter(value: string): ProjectListoToAutocomplete[] {
+    console.log('from filter')
+    const filterValue = value.toLowerCase();
+    return this.filterService.projectsListToAutocomplete.filter((option) =>
+      option.projectName.toLowerCase().includes(filterValue)
+    );
+  }
+
+  onSelection(project: ProjectListoToAutocomplete) {
+
+    console.log(project)
+    // if (this.listOfSkillsForProject.length !== 0) {
+    //   const nameListOfSkillProject = this.listOfSkillsForProject.map((el) => {
+    //     return el.name;
+    //   });
+    //   if (!nameListOfSkillProject.includes(choosenSkill.name)) {
+    //     this.listOfSkillsForProject.push(choosenSkill);
+    //   } else {
+    //     alert('Skill already choosen');
+    //   }
+    // } else {
+    //   this.listOfSkillsForProject.push(choosenSkill);
+    // }
+  }
+
+
+
 }
 
